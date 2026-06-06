354a218ad8f3f0c3f047d11a58630d6c556c9d09
avni <avnipisharody@gmail.com>
2026-06-06 13:48:13 +0000

    idk

---
+++ import os.py
@@ -0,0 +1,12 @@
+import os
+import random
+import math
+import pygame
+from os import listdir, name
+from os.path import isfile, join
+pygame.init()
+
+pygame.display.set_caption("car game thing ")
+
+WIDTH, HEIGHT = 800, 600
+FPS=60 
